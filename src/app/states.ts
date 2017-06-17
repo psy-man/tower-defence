9fdef5147edbfe08296e21020b3a462ab7e03daf
Eugene Filipyev <w.0.w@mail.ru>
2017-06-17 19:21:22 +0300

    Added health text

--- src/app/states.ts
+++ src/app/states.ts
@@ -6,19 +6,30 @@ export class States extends Container {
   public moneyText: Text;
 
   public health: number = 20;
+  public healthText: Text;
 
   public waveNumber = 1;
 
   constructor() {
     super();
 
+    this.renderMoneyText();
+    this.renderHealthText();
+  }
+
+  render() {
+    this.moneyText.text = `${this.money} points`;
+    this.healthText.text = `${this.health} health`;
+  }
+
+  private renderMoneyText() {
     this.moneyText = new Text(`${this.money} points`, {
-      align: 'right',
-      fill: '#cc00ff',
-      fontFamily: 'Arial',
-      fontSize: '20px',
-      stroke: '#FFFFFF',
-      strokeThickness: 1
+        align: 'right',
+        fill: '#cc00ff',
+        fontFamily: 'Arial',
+        fontSize: '20px',
+        stroke: '#FFFFFF',
+        strokeThickness: 1
       }
     );
 
@@ -27,13 +38,23 @@ export class States extends Container {
     this.moneyText.anchor.x = 1;
 
     this.addChild(this.moneyText);
-
-    setInterval(() => {
-      this.money++;
-    }, 1000);
   }
 
-  render() {
-    this.moneyText.text = `${this.money} points`;
+  private renderHealthText() {
+    this.healthText = new Text(`${this.health} health`, {
+        align: 'right',
+        fill: '#070aff',
+        fontFamily: 'Arial',
+        fontSize: '20px',
+        stroke: '#FFFFFF',
+        strokeThickness: 1
+      }
+    );
+
+    this.healthText.position.x = 1260;
+    this.healthText.position.y = 30;
+    this.healthText.anchor.x = 1;
+
+    this.addChild(this.healthText);
   }
 }
